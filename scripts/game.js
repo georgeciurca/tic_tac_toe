159d07f0428a906b445d4dfb86600bd9eb11b429
George Ciurca <george.ciurca@gmail.com>
2022-02-15 19:43:07 +0000

    js updates

---
+++ scripts/game.js
@@ -0,0 +1,23 @@
+function startNewGame() {
+  if (players[0].name === "" || players[1].name === "") {
+    alert("Please set names for both players");
+    return;
+  }
+  activePlayerNameElement.textContent = players[activePlayer].name;
+  gameAreaElement.style.display = "block";
+}
+
+function switchPlayer() {
+  if (activePlayer === 0) {
+    activePlayer = 1;
+  } else {
+    activePlayer = 0;
+  }
+  activePlayerNameElement.textContent = players[activePlayer].name;
+}
+
+function selectGameField(event) {
+  event.target.textContent = players[activePlayer].symbol;
+  event.target.classList.add("disabled");
+  switchPlayer();
+}
