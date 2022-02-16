6f8d1ad08e4601865d75bae0d8f3342c3d28d95e
George Ciurca <george.ciurca@gmail.com>
2022-02-16 08:50:27 +0000

    game logic updated

--- scripts/game.js
+++ scripts/game.js
@@ -17,23 +17,63 @@ function switchPlayer() {
 }
 
 function selectGameField(event) {
+  const selectedField = event.target;
+  const selectedColumn = selectedField.dataset.col - 1;
+  const selectedRow = selectedField.dataset.row - 1;
 
-    const selectedField = event.target;
-    const selectedColumn = selectedField.dataset.col - 1;
-    const selectedRow = selectedField.dataset.row - 1;
-
-    if (gameData[selectedRow][selectedColumn] > 0){
-        alert ('Please select an empty field');
+  if (gameData[selectedRow][selectedColumn] > 0) {
+    alert("Please select an empty field");
     return;
-    }
+  }
 
-    selectedField.textContent = players[activePlayer].symbol;
-    selectedField.classList.add("disabled");
+  selectedField.textContent = players[activePlayer].symbol;
+  selectedField.classList.add("disabled");
+
+  gameData[selectedRow][selectedColumn] = activePlayer + 1;
+  const winnerId = checkForGameOver();
+  console.log(winnerId);
 
 
-
-    gameData[selectedRow][selectedColumn] = activePlayer + 1;
-    console.log(gameData);
-
+  currentRound++;
   switchPlayer();
+}
+
+function checkForGameOver() {
+  for (let i = 0; i < 3; i++) {
+    if (
+      gameData[i][0] > 0 &&
+      gameData[i][0] === gameData[i][1] &&
+      gameData[i][1] === gameData[i][2]
+    ) {
+      return gameData[i][0];
+    }
+  }
+  for (let i = 0; i < 3; i++) {
+    if (
+      gameData[0][i] > 0 &&
+      gameData[0][i] === gameData[1][i] &&
+      gameData[0][i] === gameData[2][i]
+    ) {
+      return gameData[0][i];
+    }
+  }
+  if (
+    gameData[0][0] > 0 &&
+    gameData[0][0] === gameData[1][1] && 
+    gameData[1][1] && gameData[2][2]
+  ) {
+    return gameData[0][0];
+  }
+  if (
+    gameData[2][0] > 0 &&
+    gameData[2][0] === gameData[1][1] && 
+    gameData[1][1] && gameData[0][2]
+  ) {
+    return gameData[2][0];
+  }
+
+  if (currentRound === 9) {
+    return -1;
+  }
+  return 0;
 }
